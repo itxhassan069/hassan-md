3b57933c396d0ad74832b01342a8871e25f1d7f1
MR hassan <mrhassa.n799100@gmail.com>
2024-09-01 22:20:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const bot = require(__dirname + '/lib/smd')
-const { VERSION } = require(__dirname + '/config')
+const bot = require(__dirname + '/lib/smd');
+const { VERSION } = require(__dirname + '/config');
+const Debug = require('debug')('bot'); // Ensure 'debug' is required to avoid undefined reference
 
 const start = async () => {
-    Debug.info(`hassan ${VERSION}`)
-  try {
-    await bot.init()
-    bot.logger.info('⏳ Database syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
-    start();
-  }
-}
+    Debug(`hassan ${VERSION}`); // Log version using the debug instance
+    try {
+        await bot.init();
+        bot.logger.info('⏳ Database syncing!');
+        await bot.DATABASE.sync();
+        await bot.connect();
+    } catch (error) {
+        Debug.error(error);
+        start(); // Recursive call in case of an error
+    }
+};
+
 start();
